9e6c905646d44cb3cc6fed1ca2380e9233d5b094
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-10-08 11:13:06 +0200

    update

--- app.js
+++ app.js
@@ -6,21 +6,26 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// app.get("/public/images/image.jpg", (req, res) => {
-//   const filePath = path.join(__dirname, "public/images/image.jpg");
-//   try {
-//     if (fs.existsSync(filePath)) {
-//       res.sendFile(filePath);
-//     } else {
-//       res.status(404).send("Fichier non trouvé");
-//     }
-//   } catch (error) {
-//     console.error("Erreur lors de l'envoie du fichier: ", error);
-//     res.status(500).send("Erreur interne du serveur");
-//   }
-// });
-
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  // let body = "";
+  // req.on("data", (data) => {
+  //   body += data;
+  // });
+
+  // req.on("end", () => {
+  //   console.log(body);
+  //   console.log(typeof body);
+  //   res.render("index");
+  // });
+
+  console.log(req.body);
+
+  res.render("index");
+});
 
 app.get("/", (req, res) => {
   res.render("index");
