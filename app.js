6977ea54cf9702fa629478fadbb41796292bbdb9
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-10-14 09:25:46 +0200

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -9,25 +10,19 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
-app.post("/", (req, res) => {
-  // let body = "";
-  // req.on("data", (data) => {
-  //   body += data;
-  // });
-
-  // req.on("end", () => {
-  //   console.log(body);
-  //   console.log(typeof body);
-  //   res.render("index");
-  // });
-
-  console.log(req.body);
-
-  res.render("index");
+app.get(["/foo", "/index"], (req, res) => {
+  console.log("match");
+  res.end();
 });
 
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
   res.render("index");
 });
 
